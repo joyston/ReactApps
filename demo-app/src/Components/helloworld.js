ce86a8a31d3e27b5567038deed70b1d6ba60cee0
DESKTOP-H87TPR2\ASUS <joystonf@gmail.com>
2025-06-03 16:49:35 +0530

    -Practiced variables (string, number, link) in reactjs

--- demo-app/src/Components/helloworld.js
+++ demo-app/src/Components/helloworld.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
 export default function Helloworld() {
+
+    const greeting = "Welcome to React!";
+    const number = 30;
+    const link = "http://www.google.com";
+
     return (
-        <h1>Helloworld</h1>
+        <div>
+            <h1>Helloworld</h1>
+            <p>{greeting} </p>
+            <p>Number value is {number} </p>
+            <p>Number added to Number is {number + number} </p>
+            <a href={link}>Go to Google</a>
+        </div>
+
     )
 }
